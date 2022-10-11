e116735dd76438aeb903f623b5c5e3c7aee36787
Meraz Ahasan <113965568+Meraz210@users.noreply.github.com>
2022-10-11 13:47:17 +0600

    Add files via upload

---
+++ lab 3 tasks 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int i,j, arr1[3][4]={{1,6,7,9},{2,4,5,8},{3,1,9,4}};
+ for(i=0;i<4;i++)
+ {
+     for(j=0;j<3;j++)
+
+ {
+     cout<<arr1[j][i]<<" ";
+ }
+ cout<<endl;
+ }
+ return 0;
+}
