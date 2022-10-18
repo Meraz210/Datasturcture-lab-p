22f5f8b98498bfb7082c87aeab4d074106ac4838
Meraz Ahasan <113965568+Meraz210@users.noreply.github.com>
2022-10-18 13:48:38 +0600

    Add files via upload

---
+++ lab 5 tasks0 2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std ;
+class student
+
+ {
+ public :
+string name;
+    int id;
+    float cgpa;
+    int credit;
+    student()
+ {
+
+ }
+
+ Student(string name ,int id ,float cgpa,int credit)
+{
+
+}
+void  setname(string name)
+{
+    this->name=name;
+
+}
+
+ void setcpgpa(float cgpa)
+ {
+     this->cgpa=cgpa;
+ }
+void setid(int id)
+{
+  this->id=id;
+}
+void setcredit(int credit)
+{
+    this->credit=credit;
+}
+
+ };
+ int n ;
+ string name;
+ int id ;
+ int credit;
+ float cgpa;
+ cout<<"number of student"<<endl;
+ cin>>n;
+ student Sutdent(n)
+ for(int i=o;i<n;i++)
+ {
+     cout<<"student info"<<endl;
+     cout<<"student id"<<endl;
+     cin>> id;
+     cout<<"student name"<<endl;
+     cin>>name;
+     student[i]setname;
+
+
+ }
+
